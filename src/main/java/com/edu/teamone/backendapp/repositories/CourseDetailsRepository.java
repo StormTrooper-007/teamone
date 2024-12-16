5c8068332e5f0b71572ad63e9fe12e761be0e4b4
collins <chigbo.c.o@gmail.com>
2024-12-16 23:11:39 +0100

    16.12.24

--- src/main/java/com/edu/teamone/backendapp/repositories/CourseDetailsRepository.java
+++ src/main/java/com/edu/teamone/backendapp/repositories/CourseDetailsRepository.java
@@ -1,10 +1,7 @@
 package com.edu.teamone.backendapp.repositories;
 
+
 import com.edu.teamone.backendapp.models.CourseDetails;
-import com.edu.teamone.backendapp.security.AppUser;
-
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
